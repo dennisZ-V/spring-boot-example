4fcf443f50de60f03c41daa58374a3cfa70ad577
zhangdi <zhangdi@wondersoft.cn>
2024-09-05 19:09:49 +0800

    增加脚本初始化

---
+++ src/main/resources/init/init-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO t_department (dept_name) VALUES ('研发中心');
+INSERT INTO t_department (dept_name) VALUES ('产品管理部');
+INSERT INTO t_department (dept_name) VALUES ('质量管理部');
--- src/main/resources/init/init.sql
+++ src/main/resources/init/init.sql
@@ -1,30 +1,15 @@
-# spring-boot-example
-## 使用说明
-___
-该示例引入了springdoc，启动后请访问`http://localhost:9090/swagger-ui/index.html#/`
-
-### 建表语句
-```sql
-部门表
 create table if not exists t_department
 (
     dept_id   int auto_increment comment '部门id'
-        primary key,
+    primary key,
     dept_name varchar(64) not null comment '部门名称'
-)
+    )
     comment '部门表';
 
-INSERT INTO t_department (dept_name) VALUES ('研发中心');
-INSERT INTO t_department (dept_name) VALUES ('产品管理部');
-INSERT INTO t_department (dept_name) VALUES ('质量管理部');
-```
-
-```sql
-工单表
 create table if not exists t_order
 (
     id             int auto_increment
-        primary key,
+    primary key,
     order_no       varchar(64)      not null comment '工单编号',
     order_type     int              not null comment '工单类型，0-交办，1-直接答复，2-无效工单',
     title          varchar(64)      not null comment '标题',
@@ -35,8 +20,8 @@ create table if not exists t_order
     is_overdue     bit              null comment '是否超期，0-否，1-是',
     is_removed     bit default b'0' not null comment '是否删除，0-未删除，1-已删除',
     constraint t_order_order_no_uindex
-        unique (order_no)
-)
+    unique (order_no)
+    )
     comment '工单表';
 
 create index t_order_fenpai_time_index
@@ -44,4 +29,3 @@ create index t_order_fenpai_time_index
 
 create index t_order_handle_dept_id_index
     on demo.t_order (handle_dept_id);
-```
